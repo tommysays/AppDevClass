2ef9aa69d0bd8d4e75ac2e9283e9c1e39a7ae754
JOSHUA CHRISTOPHER LEE <jcl5268@m-patteew140-77.clc-labs.its.psu.edu>
2014-09-06 19:59:20 -0400

    Created Assignment 1 Project.

---
+++ Assignment 1/Assignment 1/JCLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JCLAppDelegate.h
+//  Assignment 1
+//
+//  Created by JOSHUA CHRISTOPHER LEE on 9/6/14.
+//  Copyright (c) 2014 Joshua Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JCLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Assignment 1/Assignment 1/JCLViewController.h
@@ -0,0 +1,13 @@
+//
+//  JCLViewController.h
+//  Assignment 1
+//
+//  Created by JOSHUA CHRISTOPHER LEE on 9/6/14.
+//  Copyright (c) 2014 Joshua Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JCLViewController : UIViewController
+
+@end
