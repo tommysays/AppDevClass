73e8859edf37f787f820d20f110a8dff0b878d13
JOSHUA CHRISTOPHER LEE <jcl5268@m-patteew140-77.clc-labs.its.psu.edu>
2014-09-06 22:24:44 -0400

    Did a bunch more work.

--- Assignment 1/Assignment 1/JCLViewController.m
+++ Assignment 1/Assignment 1/JCLViewController.m
@@ -7,12 +7,11 @@
 //
 
 #import "JCLViewController.h"
-static NSInteger const NUM_ANSWERS = 4;
+static NSInteger const NUM_SEGMENTS = 4;
 static NSInteger const ANSWER_DEVIATION = 5;
-static NSInteger const NUM_QUESTIONS = 7;
+static NSInteger const NUM_QUESTIONS = 10;
 static NSInteger const MIN_NUMBER = 1;
 static NSInteger const MAX_NUMBER = 15;
-NSMutableArray *array;
 
 @interface JCLViewController ()
 @property (weak, nonatomic) IBOutlet UILabel *multiplierLabel;
@@ -21,11 +20,13 @@ NSMutableArray *array;
 @property (weak, nonatomic) IBOutlet UILabel *numCorrectLabel;
 @property (weak, nonatomic) IBOutlet UILabel *correctLabel;
 @property (weak, nonatomic) IBOutlet UISegmentedControl *answersControl;
+@property (weak, nonatomic) IBOutlet UIButton *nextButton;
 - (IBAction)answerSelected:(id)sender;
 - (IBAction)nextPressed:(id)sender;
 
-@property NSInteger numCorrect = 0;
-@property NSInteger numQuestion = 0;
+@property NSInteger numCorrect;
+@property NSInteger numQuestion;
+@property NSInteger answer;
 
 @end
 
@@ -35,8 +36,12 @@ NSMutableArray *array;
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    [self.answersControl removeAllSegments];
+    for (int i = 0; i < NUM_SEGMENTS; ++i){
+        [self.answersControl insertSegmentWithTitle:@"" atIndex:0 animated:false];
+    }
     [self clearAll];
-    
+    [self.nextButton setTitle:@"Start" forState:UIControlStateNormal];
 }
 
 - (void)clearAll{
@@ -45,30 +50,56 @@ NSMutableArray *array;
     self.resultLabel.text = @"";
     self.numCorrectLabel.text = @"";
     self.correctLabel.text = @"";
-    for (int i = 0; i < NUM_ANSWERS; i++) {
+    for (int i = 0; i < NUM_SEGMENTS; i++) {
         [self.answersControl setTitle:@"" forSegmentAtIndex:i];
     }
+    self.numCorrect = 0;
+    self.numQuestion = 0;
+    self.answer = 0;
 }
 
-- (void)multiply{
-    
+- (void)generateProblem{
+    int multiplier = arc4random_uniform(MAX_NUMBER) + MIN_NUMBER;
+    int multiplicand = arc4random_uniform(MAX_NUMBER) + MIN_NUMBER;
+    self.answer = multiplier * multiplicand;
+    self.multiplierLabel.text = [NSString stringWithFormat:@"%d", multiplier];
+    self.multiplicandLabel.text = [NSString stringWithFormat:@"%d", multiplicand];
     
 }
 
 - (BOOL)isCorrect{
-    return true;
+    int index = self.answersControl.selectedSegmentIndex;
+    if (self.answer == [[self.answersControl titleForSegmentAtIndex:index] intValue]){
+        return true;
+    }
+    return false;
 }
 
 - (IBAction)answerSelected:(id)sender {
     if (self.isCorrect){
         self.numCorrect++;
-        self.answersControl.enabled = false;
-        self.numCorrectLabel.text = @" Questions Correct";
         self.correctLabel.text = @"Correct!";
+    } else{
+        self.correctLabel.text = @"Incorrect.";
     }
+    self.answersControl.enabled = false;
+    self.resultLabel.text = [NSString stringWithFormat:@"%d", self.answer];
+    self.numCorrectLabel.text = [NSString stringWithFormat:@"%d/%d Questions Correct", self.numCorrect, self.numQuestion];
 }
 
 - (IBAction)nextPressed:(id)sender {
-    
+    if (self.numQuestion == 0){
+        [self generateProblem];
+        [self.nextButton setTitle:@"Next" forState:UIControlStateNormal];
+        self.numQuestion++;
+    } else if (self.numQuestion < NUM_QUESTIONS - 1){
+        [self generateProblem];
+        self.numQuestion++;
+    } else if (self.numQuestion < NUM_QUESTIONS){
+        [self.nextButton setTitle:@"Reset" forState:UIControlStateNormal];
+    } else{
+        [self clearAll];
+        [self.nextButton setTitle:@"Start" forState:UIControlStateNormal];
+    }
 }
 @end
