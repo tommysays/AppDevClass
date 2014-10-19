c83e591c488600ff15d8bb417d9232b332bb34de
JOSHUA CHRISTOPHER LEE <jcl5268@m-willard109-195.clc-labs.its.psu.edu>
2014-10-19 16:18:16 -0400

    Created project PhotoViewer.

---
+++ PhotoViewer/PhotoViewer/JCLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JCLAppDelegate.h
+//  PhotoViewer
+//
+//  Created by JOSHUA CHRISTOPHER LEE on 10/19/14.
+//  Copyright (c) 2014 Joshua Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JCLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PhotoViewer/PhotoViewer/JCLViewController.h
@@ -0,0 +1,13 @@
+//
+//  JCLViewController.h
+//  PhotoViewer
+//
+//  Created by JOSHUA CHRISTOPHER LEE on 10/19/14.
+//  Copyright (c) 2014 Joshua Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JCLViewController : UIViewController
+
+@end
