fe761bab44f2b09ba481d5b91e86d737a7d6bdb6
JOSHUA CHRISTOPHER LEE <jcl5268@m-willard109-195.clc-labs.its.psu.edu>
2014-10-19 23:07:18 -0400

    Everything but zoom is working.

--- PhotoViewer/PhotoViewer/JCLViewController.m
+++ PhotoViewer/PhotoViewer/JCLViewController.m
@@ -8,13 +8,20 @@
 
 #import "JCLViewController.h"
 
-@interface JCLViewController () <UIScrollViewDelegate>
+@interface JCLViewController () <UIScrollViewDelegate, UIGestureRecognizerDelegate>
 @property (weak, nonatomic) IBOutlet UIScrollView *mainScrollView;
+@property (weak, nonatomic) IBOutlet UIImageView *arrowUp;
+@property (weak, nonatomic) IBOutlet UIImageView *arrowDown;
+@property (weak, nonatomic) IBOutlet UIImageView *arrowLeft;
+@property (weak, nonatomic) IBOutlet UIImageView *arrowRight;
+
 
 @property (strong, nonatomic) JCLModel *model;
 @property (strong, nonatomic) UIScrollView *zoomScroll;
+@property (strong, nonatomic) UIImageView *zoomImage;
 @property NSInteger curSet; // What set we are showing
 @property NSInteger curImageIndex; // Index of the image being shown.
+@property CGPoint lastOffset;
 
 @property CGPoint dragStart;
 @property BOOL isAtTop;
@@ -22,8 +29,11 @@
 @property BOOL moveHorizontal;
 @property BOOL isZoomed;
 
+@property NSTimer *arrowTimer;
+
 @end
 
+const NSTimeInterval kArrowFadeTime = 1.0;
 const CGFloat kLabelHeight = 4; // Higher numbers = higher label.
 const CGFloat kMinZoomScale = 1.0;
 const CGFloat kMaxZoomScale = 10.0;
@@ -46,6 +56,7 @@ const CGFloat kMaxZoomScale = 10.0;
     
     [self.model initImages];
     [self setupScrollView];
+    [self resetArrowsWithX:0 andY:0];
 }
 
 - (void) setupScrollView{
@@ -53,16 +64,32 @@ const CGFloat kMaxZoomScale = 10.0;
     NSInteger width = [self.model numberOfSets];
     NSInteger height = [self.model maxSizeOfSet];
     
+    UIPinchGestureRecognizer *recognizer = [[UIPinchGestureRecognizer alloc] initWithTarget:self action:@selector(handlePinch:)];
+    recognizer.delegate = self;
+    [self.view addGestureRecognizer:recognizer];
+    
+    
     self.zoomScroll = [[UIScrollView alloc] init];
-    self.zoomScroll.delegate = self;
+    [self.view addSubview:self.zoomScroll];
     self.zoomScroll.minimumZoomScale = kMinZoomScale;
     self.zoomScroll.maximumZoomScale = kMaxZoomScale;
+    self.zoomScroll.frame = self.view.frame;
+    //self.zoomScroll.contentSize = CGSizeMake(1000, 1000);
+    self.zoomScroll.contentSize = self.zoomScroll.bounds.size;
+    self.zoomScroll.bounces = true;
+    self.zoomScroll.backgroundColor = [UIColor blackColor];
+    self.zoomScroll.hidden = true;
+    self.zoomScroll.delegate = self;
     
-    self.mainScrollView.minimumZoomScale = kMinZoomScale;
-    self.mainScrollView.maximumZoomScale = kMinZoomScale + 1;
+    recognizer = [[UIPinchGestureRecognizer alloc] initWithTarget:self action:@selector(handlePinch:)];
+    recognizer.delegate = self;
+    [self.zoomScroll addGestureRecognizer:recognizer];
+    
     [self.mainScrollView setContentSize:CGSizeMake(viewSize.width * width, viewSize.height * height)];
     self.mainScrollView.pagingEnabled = YES;
     self.mainScrollView.directionalLockEnabled = YES;
+    self.mainScrollView.minimumZoomScale = kMinZoomScale;
+    self.mainScrollView.maximumZoomScale = kMinZoomScale;
     self.mainScrollView.delegate = self;
     [self loadImages];
 }
@@ -95,48 +122,95 @@ const CGFloat kMaxZoomScale = 10.0;
     }
 }
 
-#pragma mark ScrollView Delegate Methods
+#pragma mark Arrow Methods
 
-- (UIView *) viewForZoomingInScrollView:(UIScrollView *)scrollView{
-    CGFloat xOffset = [scrollView contentOffset].x;
-    CGFloat yOffset = [scrollView contentOffset].y;
-    xOffset /= self.mainScrollView.bounds.size.width;
-    yOffset /= self.mainScrollView.bounds.size.height;
-    CGRect frame = CGRectMake(0, 0, self.mainScrollView.bounds.size.width, self.mainScrollView.bounds.size.height);
-    UIImageView *imgView = [[UIImageView alloc] initWithFrame:frame];
-    imgView.image = [self.model image:yOffset fromSet:xOffset];
-    return imgView;
+- (void) resetArrowsWithX:(CGFloat)xOffset andY:(CGFloat)yOffset{
+    [self.arrowTimer invalidate];
+    [self hideArrows];
+    if (yOffset < [self.model sizeOfSet:xOffset] - 1){
+        self.arrowDown.hidden = false;
+    }
+    if (yOffset > 0){
+        self.arrowUp.hidden = false;
+    } else{
+        if (xOffset < [self.model numberOfSets] - 1){
+            self.arrowRight.hidden = false;
+        }
+        if (xOffset > 0){
+            self.arrowLeft.hidden = false;
+        }
+    }
+    // After a bit, timer will hide all arrows.
+    self.arrowTimer = [NSTimer scheduledTimerWithTimeInterval:kArrowFadeTime target:self selector:@selector(hideArrows) userInfo:nil repeats:NO];
 }
 
-- (void) scrollViewWillBeginZooming:(UIScrollView *)scrollView withView:(UIView *)view{
-    if (self.mainScrollView == scrollView){
-        return;
-    }
-    NSLog(@"b");
-    CGFloat xOffset = [scrollView contentOffset].x;
-    CGFloat yOffset = [scrollView contentOffset].y;
-    xOffset /= scrollView.bounds.size.width;
-    yOffset /= scrollView.bounds.size.height;
-    CGRect frame = CGRectMake(0, 0, scrollView.bounds.size.width, scrollView.bounds.size.height);
-    UIImageView *imgView = [[UIImageView alloc] initWithFrame:frame];
-    imgView.image = [self.model image:yOffset fromSet:xOffset];
-    self.zoomScroll.frame = frame;
-    self.zoomScroll.contentSize = imgView.image.size;
-    [self.view addSubview:self.zoomScroll];
+- (void) hideArrows{
+    self.arrowUp.hidden = true;
+    self.arrowDown.hidden = true;
+    self.arrowLeft.hidden = true;
+    self.arrowRight.hidden = true;
+}
+
+#pragma mark ScrollView Zoom
+
+- (UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView{
+    return self.zoomImage;
 }
 
 - (void) scrollViewDidEndZooming:(UIScrollView *)scrollView withView:(UIView *)view atScale:(CGFloat)scale{
-    if (scale == kMinZoomScale){
-        [self.zoomScroll removeFromSuperview];
-    } else{
-        self.isZoomed = YES;
+    if (scale == 1.0){
+        self.zoomScroll.hidden = true;
+        self.mainScrollView.hidden = false;
     }
 }
+
+- (IBAction)handlePinch:(UIPinchGestureRecognizer *)recognizer{
+    
+    if (self.zoomScroll.hidden == true){
+        self.zoomScroll.hidden = false;
+        self.mainScrollView.hidden = true;
+        self.zoomImage = [[UIImageView alloc] initWithFrame:self.zoomScroll.frame];
+        CGFloat xOffset = [self.mainScrollView contentOffset].x;
+        CGFloat yOffset = [self.mainScrollView contentOffset].y;
+        xOffset /= self.mainScrollView.bounds.size.width;
+        yOffset /= self.mainScrollView.bounds.size.height;
+        self.zoomImage.image = [self.model image:yOffset fromSet:xOffset];
+        self.zoomImage.contentMode = UIViewContentModeScaleAspectFit;
+        [self.zoomScroll addSubview:self.zoomImage];
+    }
+    NSLog(@"Recognizer: %f", recognizer.scale);
+    [self.zoomScroll setZoomScale:recognizer.scale animated:YES];
+    NSLog(@"ZS: %f", self.zoomScroll.zoomScale);
+    /*
+    if (self.zoomScroll.zoomScale == 1.0){
+        self.zoomScroll.hidden = true;
+    }
+     */
+}
+
+
+- (void) disableMainScrollView{
+    self.mainScrollView.userInteractionEnabled = false;
+    self.mainScrollView.hidden = true;
+}
+
+- (void) enableMainScrollView{
+    self.mainScrollView.userInteractionEnabled = true;
+    self.mainScrollView.hidden = false;
+    self.mainScrollView.contentOffset = self.lastOffset;
+}
+
+#pragma mark ScrollView Drag
 
 - (void) scrollViewWillBeginDragging:(UIScrollView *)scrollView{
     self.moveHorizontal = false;
     self.moveVertical = false;
     self.dragStart = scrollView.contentOffset;
+    CGFloat xOffset = [scrollView contentOffset].x;
+    CGFloat yOffset = [scrollView contentOffset].y;
+    xOffset /= scrollView.bounds.size.width;
+    yOffset /= scrollView.bounds.size.height;
+    [self resetArrowsWithX:xOffset andY:yOffset];
 }
 
 - (void) scrollViewDidEndDecelerating:(UIScrollView *)scrollView{
@@ -152,6 +226,8 @@ const CGFloat kMaxZoomScale = 10.0;
     CGFloat width = scrollView.contentSize.width;
     CGFloat height = scrollView.bounds.size.height * [self.model sizeOfSet:xOffset];
     scrollView.contentSize = CGSizeMake(width, height);
+    
+    [self resetArrowsWithX:xOffset andY:yOffset];
 }
 
 - (void) scrollViewDidScroll:(UIScrollView *)scrollView{
@@ -168,6 +244,11 @@ const CGFloat kMaxZoomScale = 10.0;
             scrollView.contentOffset = CGPointMake(scrollView.contentOffset.x, self.dragStart.y);
         }
     }
+    CGFloat xOffset = [scrollView contentOffset].x;
+    CGFloat yOffset = [scrollView contentOffset].y;
+    xOffset /= scrollView.bounds.size.width;
+    yOffset /= scrollView.bounds.size.height;
+    [self resetArrowsWithX:xOffset andY:yOffset];
 }
 
 
