eb20e780acf728bd7eb6996ec4f1160c0cfeaf1e
JOSHUA CHRISTOPHER LEE <jcl5268@m-willard109-195.clc-labs.its.psu.edu>
2014-11-13 11:04:25 -0500

    Initial commit for final project (Super Tic Tac Toe, abbreviated SuperT).

---
+++ SuperT/SuperT/JCLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JCLAppDelegate.h
+//  SuperT
+//
+//  Created by JOSHUA CHRISTOPHER LEE on 11/13/14.
+//  Copyright (c) 2014 Joshua Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JCLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
