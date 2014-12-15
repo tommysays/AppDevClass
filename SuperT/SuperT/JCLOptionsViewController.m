48ce8f488cd8073596497dc036e043ba9cf24c1c
JOSHUA CHRISTOPHER LEE <jcl5268@m-willard109-197.clc-labs.its.psu.edu>
2014-12-14 20:55:29 -0500

    Fixed the Options theme bug. App is feature-complete and mostly bug free!

--- SuperT/SuperT/JCLOptionsViewController.m
+++ SuperT/SuperT/JCLOptionsViewController.m
@@ -13,7 +13,7 @@
 @interface JCLOptionsViewController ()
 @property (weak, nonatomic) IBOutlet UISwitch *soundSwitch;
 @property (weak, nonatomic) IBOutlet UISlider *volumeSlider;
-@property (weak, nonatomic) IBOutlet UIView *highlightView;
+@property (strong, nonatomic) IBOutletCollection(UIView) NSArray *highlights;
 @property (strong, nonatomic) IBOutletCollection(UIButton) NSArray *themeButtons;
 @property JCLModel *model;
 @property SoundManager *soundManager;
@@ -27,34 +27,60 @@
 #pragma mark - Initialization
 
 - (void) viewDidLoad{
+    [super viewDidLoad];
     self.model = [JCLModel sharedInstance];
     self.soundManager = [SoundManager sharedInstance];
     [self.soundSwitch setOn:[self.model isSoundOn]];
     [self.volumeSlider setValue:[self.model volume]];
     NSInteger curTheme = [self.model currentTheme];
-    for (UIButton *button in self.themeButtons){
-        if (button.tag == curTheme){
-            self.highlightView.center = button.center;
-            NSLog(@"center = %@", NSStringFromCGPoint(self.highlightView.center));
-            NSLog(@"tag = %d", button.tag);
-            break;
+    for (UIView *view in self.highlights){
+        if (view.tag == curTheme){
+            view.hidden = NO;
+        } else{
+            view.hidden = YES;
         }
     }
+    /*
+    for (UIButton *button in self.themeButtons){
+        if (button.tag == curTheme){
+            CGSize size = self.highlightView.frame.size;
+            CGPoint origin = self.highlightView.frame.origin;
+            CGPoint buttonOrigin = button.frame.origin;
+            CGRect frame = CGRectMake(origin.x, buttonOrigin.y, size.width, size.height);
+            self.highlightView.frame = frame;
+
+            //[self.highlightView setCenter:button.center];
+            //NSLog(@"will appear center: %@", NSStringFromCGPoint(self.highlightView.center));
+            //break;
+        }
+    }
+     */
 }
 
 - (void) viewDidAppear:(BOOL)animated{
-    NSLog(@"real center = %@", NSStringFromCGPoint(self.highlightView.center));
-    NSLog(@"---");
+//    NSLog(@"real center = %@", NSStringFromCGPoint(self.highlightView.center));
+//    NSLog(@"---");
+//    NSLog(@"Theme %d", [self.model currentTheme]);
+    
 }
 
 - (void) viewWillAppear:(BOOL)animated{
+           // NSLog(@"%@", NSStringFromCGRect(self.highlightView.frame));
+    /*
     NSInteger curTheme = [self.model currentTheme];
     for (UIButton *button in self.themeButtons){
         if (button.tag == curTheme){
-            self.highlightView.center = button.center;
-            break;
+            CGSize size = self.highlightView.frame.size;
+            CGPoint origin = self.highlightView.frame.origin;
+            CGPoint buttonOrigin = button.frame.origin;
+            CGRect frame = CGRectMake(origin.x, buttonOrigin.y, size.width, size.height);
+            self.highlightView.frame = frame;
+            //[self.highlightView setCenter:button.center];
+            //NSLog(@"will appear center: %@", NSStringFromCGPoint(self.highlightView.center));
+            //break;
         }
     }
+     */
 }
 
 #pragma mark - Button Reactions
@@ -63,7 +89,14 @@
     [self.soundManager playConfirmButton];
     UIButton *button = (UIButton *)sender;
     [self.model updateTheme:button.tag];
-    self.highlightView.center = button.center;
+    for (UIView *view in self.highlights){
+        if (view.tag == button.tag){
+            view.hidden = NO;
+        } else{
+            view.hidden = YES;
+        }
+    }
+    //self.highlightView.center = button.center;
 }
 
 - (IBAction)backButtonPressed:(id)sender {
